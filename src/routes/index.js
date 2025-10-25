c85209be7af88c61037f86590b681959787c9bd4
Jacob <jacoblm49@gmail.com>
2025-10-25 11:59:59 -0500

    feat(22): endpoint roles

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const categoriesRouter = require('./categories.routes'); 
 const movementsRouter = require('./movements.routes');
+const rolesRouter = require('./roles.routes');
 
 function routerApi(app) {
   app.use('/categories', categoriesRouter);
   app.use('/movements', movementsRouter);
+  app.use('/roles', rolesRouter);
 }
 
 module.exports = routerApi;
