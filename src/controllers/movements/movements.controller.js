2941debe96576a859cba0976fa8c81e75dc86ab4
jacoblm49 <jacoblm49@gmail.com>
2025-10-30 14:16:54 -0500

    feat(endpoint): endpoints terminados para movimientos y correciones foreign key

--- src/controllers/movements/movements.controller.js
+++ src/controllers/movements/movements.controller.js
@@ -1,4 +1,4 @@
-const { createMovement }  = require ('../../services/movements/movements.service')
+const { createMovement, getMovements, getMovementById, updateMovement, deleteMovement }  = require ('../../services/movements/movements.service')
 
 const createMovementController = async (req, res) => {
     try{
@@ -13,4 +13,50 @@ const createMovementController = async (req, res) => {
     }
 }
 
-module.exports={createMovementController};
+const getMovementsController = async (req, res) => {
+  try {
+    const movements = await getMovements();
+    res.json(movements);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getMovementByIdController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movement = await getMovementById(id);
+    res.json(movement);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const updateMovementController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const updatedMovement = await updateMovement(id, data);
+    res.json({
+      message: 'Movimiento actualizado correctamente',
+      data: updatedMovement
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteMovementController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedMovement = await deleteMovement(id);
+    res.json({
+      message: 'Movimiento eliminado correctamente',
+      data: deletedMovement
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports={createMovementController, getMovementsController, getMovementByIdController, updateMovementController, deleteMovementController};
--- src/services/movements/movements.service.js
+++ src/services/movements/movements.service.js
@@ -5,4 +5,39 @@ const createMovement= async (data) => {
     return newMovement;
 }
 
-module.exports={createMovement};
+const getMovements = async () => {
+  const movements = await Movements.findAll({
+    include: ['Products', 'Users']
+  });
+  return movements;
+};
+
+const getMovementById = async (id) => {
+  const movement = await Movements.findByPk(id, {
+    include: ['Products', 'Users']
+  });
+  if (!movement) throw new Error('Movimiento no encontrado');
+  return movement;
+};
+
+const updateMovement = async (id, data) => {
+  const movement = await Movements.findByPk(id);
+  if (!movement) throw new Error('Movimiento no encontrado');
+  await movement.update(data);
+  return movement;
+};
+
+const deleteMovement = async (id) => {
+  const movement = await Movements.findByPk(id);
+  if (!movement) throw new Error('Movimiento no encontrado');
+  await movement.destroy();
+  return movement;
+};
+
+module.exports = {
+  createMovement,
+  getMovements,
+  getMovementById,
+  updateMovement,
+  deleteMovement
+};
